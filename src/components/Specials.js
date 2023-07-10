6b636d11e2911b5b4ca92b0fcf18741f11de1cfa
Riwokib <riwellkib@gmail.com>
2023-07-11 00:32:22 +0300

    category filter button styling css

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -3,9 +3,25 @@ import { NavLink } from 'react-router-dom';
 import "../components/styles/Specials.css"
 import SpecialsCard from './SpecialsCard'
 import specials from '../specialMenu'
+import Categories from './Categories'
+
+const allCategories = ['All' , ...new Set(specials.map((item) => item.category))];
 
 const Specials = () => {
   const [specialMenu, setSpecialMenu] = useState(specials); 
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'All'){
+      setSpecialMenu(specials);
+      return;
+    }
+
+    const newItems = category.filter((item) => item.category === category);
+
+    setSpecialMenu(newItems);
+
+  }
 
   return (
     <div className="specials-section-container">
@@ -15,6 +31,8 @@ const Specials = () => {
               <NavLink to="/menu"><button className="btn btn-menu">Online Menu</button></NavLink>
             </div>
       </div>
+
+      <Categories categories={categories} filterItems={filterItems} />
       <SpecialsCard specialItems = {specialMenu}/>
     </div>
   )
--- src/specialMenu.js
+++ src/specialMenu.js
@@ -1,12 +1,17 @@
 import Dessert1 from "./components/assets/salad.jpg"
 import Dessert2 from "./components/assets/bruschetta1.jpg"
 import Dessert3 from "./components/assets/creme.jpg"
+import item1 from "./components/assets/item-1.jpeg"
+import item2 from "./components/assets/item-2.jpeg"
+import item3 from "./components/assets/item-3.jpeg"
+import item4 from "./components/assets/item-4.jpeg"
 
 const specials =  [
     {   
         id: 1,
         image: Dessert1,
         title: "Greek Salad",
+        category: 'Monday',
         price: "$12.99",
         description: "The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
         order: "Order a delivery"
@@ -15,6 +20,7 @@ const specials =  [
         id: 2,
         image: Dessert2,
         title: "Bruschetta",
+        category: 'Teusday',
         price: "$5.99",
         description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil; made perfect for an evening dinner.",
         order: "Order a delivery"
@@ -23,10 +29,47 @@ const specials =  [
         id: 3,
         image: Dessert3,
         title: "Lemon Dessert",
+        category: 'Wednesday',
         price: "$5.00",
         description: "This comes straight from Grandma's recipe book. Every last ingredient has been sourced and is as authentic as can be imagined.",
         order: "Order a delivery"
-    }
+    },
+    {
+        id: 4,
+        title: 'buttermilk pancakes',
+        category: 'Thursday',
+        price: 15.99,
+        img: item1,
+        description: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed ",
+        order: "Order a delivery"
+      },
+      {
+        id: 5,
+        title: 'diner double',
+        category: 'Friday',
+        price: 13.99,
+        image: item2,
+        description: " vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats ",
+        order: "Order a delivery"
+      },
+      {
+        id: 6,
+        title: 'godzilla milkshake',
+        category: 'Saturday',
+        price: 6.99,
+        image:item3,
+        description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+        order: "Order a delivery"
+      },
+      {
+        id: 7,
+        title: 'country delight',
+        category: 'Sunday',
+        price: 20.99,
+        image:item4,
+        description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, ",
+        order: "Order a delivery"
+      }
 ]
 
 export default specials;
